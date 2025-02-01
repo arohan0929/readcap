6ecdfe73dda690d15508e319dc734970c4fffc1d
mayank-goel-123 <mayank.goel.work@gmail.com>
2025-02-01 16:42:11 -0500

    MongoDB connected

--- backend/db.py
+++ backend/db.py
@@ -1,5 +1,21 @@
-def send_message_DB(message):
+from datetime import datetime, timezone
+from pymongo import MongoClient
+
+client = MongoClient(MONGO_URI)
+
+database = client["readcap"]
+collection = database["messages"]
+
+
+def send_message_DB(message, user):
+    new_message = {"name": user, "message_content": message, "message_type": "text", "time": datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ")}
+    insert_result = collection.insert_one(new_message)
+    print("Inserted document ID:", insert_result.inserted_id)
     return
 
 def get_messages():
-    return
+    messages = list(collection.find({}, {"_id": 0}))  # Exclude MongoDB's _id field
+    return messages
+
+
+send_message_DB("hi", "mayank")
